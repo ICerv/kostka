696e220809da8e93f9bfbb3233e259dea94d4fad
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-04-12 01:42:05 +0200

    Event listener

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log('funguju!');
+
+let side = 1;
+
+const dice = document.querySelector('.dice');
+
+document.addEventListener('keydown', () => {
+  side += 1;
+  if (side > 6) {
+    side = 1;
+  }
+  dice.src = `img/side${side}.svg`;
+});
+
+
